79da6795af48d3c297a84a32d9e5110926ba93c6
Sebastian <pichlmeier.sebastian@gmail.com>
2019-03-03 21:49:59 +0100

    Initial interface definition

---
+++ README.md
@@ -0,0 +1,5 @@
+# LibRayMarching
+The LibRayMarching is a simple library that implements ray marching in c++. It supports scene handling and implements several primitives such as sphere, box, plane and capsuel.
+
+### Language Bindings
+The API is defined by and ACT xml. This allows powerfull binding generation for C, C++, Python and many more.
---
+++ build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+OldPath=$(pwd)
+ScriptFile=$(readlink -f $0)
+ScriptPath=$(dirname $ScriptFile)
+Build=$ScriptPath'/build'
+ACTResult=$ScriptPath'/LibRayMarching_component'
+
+case $1 in
+act)
+	cd $ScriptPath
+	act libraymarching.act.xml
+	cp $ACTResult/Implementations/Cpp/Interfaces/libraymarching* source/
+;;
+compile)
+	mkdir -p $Build
+	cd $Build
+	cmake ..
+	make
+	mv liblibraymarching.so libraymarching.so
+;;
+package)
+  echo "push button"
+  gpio -g write $light 1
+  gpio -g write $off 1
+  sleep 1
+  echo "release button"
+  gpio -g write $light 0
+  gpio -g write $off 0
+;;
+*)
+  echo "Useage: build (act|compile|package)"
+esac
+
+cd $OldPath
